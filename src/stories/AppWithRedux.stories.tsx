fafee36d917078efa5f1bbde25dbbf5cd1b41068
1Molot <sinkevich1990@gmail.com>
2023-05-17 22:42:35 +0300

    fix:it-incubator-todolist-ts-ned4

---
+++ src/stories/AppWithRedux.stories.tsx
@@ -0,0 +1,29 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import {Button} from './Button';
+import {Task} from "../Task";
+import {TaskType} from "../Todolist";
+import {action} from "@storybook/addon-actions";
+import {EditableSpan} from "../EditableSpan";
+import AppWithRedux from "../AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "../state/reduxStoreProviderDecorator";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta: Meta<typeof AppWithRedux> = {
+    title: 'TODOLISTS/AppWithRedux',
+    component: AppWithRedux,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    decorators: [ReduxStoreProviderDecorator]
+};
+
+export default meta;
+type Story = StoryObj<typeof AppWithRedux>;
+
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+export const AppWithReduxStory: Story = {};
+
